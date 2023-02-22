ed95afe03fd9ad125ab8de532a2e9b6e63e4f4e2
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-22 16:53:14 -0300

    add: basic types in typescript

---
+++ lesson_01/build/js/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let stringArr = ['a', 'b', 'c'];
+let guitars = ['lesPaul', 'Strat', 5150];
+let mixedData = ['ABC', 123, true];
---
+++ lesson_01/src/main.ts
@@ -0,0 +1,5 @@
+let stringArr: string[] = ['a', 'b', 'c']
+
+let guitars: (string | number)[] = ['lesPaul', 'Strat', 5150];
+
+let mixedData: (string | number | boolean)[]= ['ABC', 123, true]
